a1630331e3b786b1977388b059699e094d773279
= <=>
2022-07-08 17:03:24 +0900

    테스트 컨트롤러 삭제함

--- docmall/src/main/java/com/docmall/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-package com.docmall.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-@Service
-@Controller
-public class TestController {
-/*ㅇㅇ*/
-}
--- docmall/src/main/java/com/docmall/controller/TestController3.java
+++
@@ -1,8 +0,0 @@
-package com.docmall.controller;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TestController3 {
-
-}
